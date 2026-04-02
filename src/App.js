69917a9bfae2682015a2f7f0e494cd724be2b5a9
Jean <jean13franca@gmail.com>
2026-04-02 15:49:39 -0300

    Commit Inicial: Preparada a base inicial do projeto

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <main className="app">
+      <section className="card">
+        <span className="eyebrow">Desafio Tecnico Smart Nx</span>
+      </section>
+    </main>
+  );
+}
+
+export default App;
